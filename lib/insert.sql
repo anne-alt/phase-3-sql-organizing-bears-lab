39ffe4442d4539f43cd0967a105bc13f86facffa
Anne Ogongo <anneogongo98@gmail.com>
2022-11-07 01:34:06 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES 
+("Mr.Chocolate", 2, "M", "red", "happy", true),
+("Rowdy", 5, "M", "orange", "sad", false),
+("Tabitha", 7, "F", "black", "angry", true),
+("Sergeant Brown", 9, "M", "white", "happy", false),
+("Melissa", 11, "F", "red", "happy", true),
+("Grinch", 13, "M", "orange", "angry", false),
+("Wendy", 15, "F", "black", "fear", true),
+(NULL, 17, "M", "white", "happy", false);
+
